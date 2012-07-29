769ed868a0b83a9b0b3b32e3acb1d5ce7e8a3298
pinyarats <pinyarat.s@me.com>
2012-07-29 23:54:54 +0300

    Try out new 'Filter' dialog
    (not functioning under 'Service' and 'Volunteers' tab yet)

--- caloomhtml/UI/docs/jquery-ui/js/demo.js
+++ caloomhtml/UI/docs/jquery-ui/js/demo.js
@@ -22,7 +22,8 @@ $(function () {
     // Dialog
     $('#dialog').dialog({
         autoOpen: false,
-        width: 600,
+        width: 240,
+        position: [300,80],
         buttons: {
             "Ok": function () {
                 $(this).dialog("close");
@@ -33,12 +34,13 @@ $(function () {
         }
     });
 
+
     // Dialog Link
     $('#dialog_link').click(function () {
         $('#dialog').dialog('open');
         return false;
     });
-
+    
     // Modal Link
     $('#modal_link').click(function () {
         $('#dialog-message').dialog('open');
