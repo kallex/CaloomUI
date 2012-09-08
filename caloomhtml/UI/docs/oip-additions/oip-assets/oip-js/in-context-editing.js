16665b155528706addd84bc4b3f5f5b633c7170f
Kalle Launiala <kalle.launiala@protonit.net>
2012-09-08 18:59:25 +0300

    Fixed some of the modal editor parts on excerpt and Text_Long

--- caloomhtml/UI/docs/oip-additions/oip-assets/oip-js/in-context-editing.js
+++ caloomhtml/UI/docs/oip-additions/oip-assets/oip-js/in-context-editing.js
@@ -9,3 +9,15 @@ $(document).on("click", ".open-IncontextEditDialog_Text_Short", function () {
 
 $('#IncontextEditDialog_Text_Short').modal('show');
 });
+
+$(document).on("click", ".open-IncontextEditDialog_Text_Long", function () {
+    var fieldid = $(this).data('id');
+    var fieldtext = $(this).text();
+
+
+    $(".modal-body #ObjectFieldID").val(fieldid);
+    $(".modal-body #Text_Long").val(fieldtext);
+
+
+$('#IncontextEditDialog_Text_Long').modal('show');
+});
