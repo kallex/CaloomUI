cf33883abeb336fb8e485ae7a3cd54e43f264ea5
Kalle Launiala <kalle.launiala@protonit.net>
2012-09-14 17:42:36 +0300

    Updated the remove confirmation modal

--- caloomhtml/UI/docs/oip-additions/oip-assets/oip-js/modal-id-actions.js
+++ caloomhtml/UI/docs/oip-additions/oip-assets/oip-js/modal-id-actions.js
@@ -1,11 +1,10 @@
 $(document).on("click", ".open-IDAction_ConfirmRemove", function () {
     var fieldid = $(this).data('id');
     var fieldquestion = $(this).data('question');
-    var fieldtext = $(this).text();
 
 
-    $(".modal-body #ObjectFieldID").val(fieldid + "_Action_Remove");
-    $(".modal-body #question").val(fieldquestion);
+    $('.modal-body #ObjectFieldID').val(fieldid);
+    $('.modal-body #question').val(fieldquestion);
 
 
     $('#IDAction_ConfirmRemove').modal('show');
