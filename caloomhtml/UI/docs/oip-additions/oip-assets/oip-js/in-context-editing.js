e33ffeb09f12211346aa62c5161de519351e89a2
Kalle Launiala <kalle.launiala@protonit.net>
2012-12-13 14:30:25 +0200

    Fixed in-context editing, added account container/reference updates from root

--- caloomhtml/UI/docs/oip-additions/oip-assets/oip-js/in-context-editing.js
+++ caloomhtml/UI/docs/oip-additions/oip-assets/oip-js/in-context-editing.js
@@ -5,7 +5,7 @@ $(document).on("click", ".open-IncontextEditDialog_Text_Short", function () {
     var fieldid = $(this).data('id');
     var fieldtext = $(this).text();
 
-    $(".modal-body #ObjectFieldID").val(fieldid);
+    $(".modal-body #InContextEditFieldID").val(fieldid);
     $(".modal-body #Text_Short").val(fieldtext);
     $('#IncontextEditDialog_Text_Short').modal('show');
 });
@@ -18,7 +18,7 @@ $(document).on("click", ".open-IncontextEditDialog_Text_Long", function () {
     //var fieldtext = $(this).text();
     var fieldtext = $(this).html();
 
-    $(".modal-body #ObjectFieldID").val(fieldid);
+    $(".modal-body #InContextEditFieldID").val(fieldid);
     $(".modal-body #Text_Long").val(fieldtext);
     addRichTextEditTextLong();
     $('#IncontextEditDialog_Text_Long').modal('show');
