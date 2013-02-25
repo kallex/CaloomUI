7f1b6b1be971500e5a64124e79eb13adfa752a1d
Kalle Launiala <kalle.launiala@protonit.net>
2013-02-25 08:00:07 +0200

    Imagegroup selection + edit partially done (not functioning properly yet)

---
+++ caloomhtml/UI/docs/oip-additions/oip-assets/oip-js/oip-imagegroup.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $(".oipimageselector").on("click", function () {
+        var imgid = $(this).data("id");
+        var imgsrc = $(this).data("src");
+        var imgcaption = $(this).data("caption");
+        var imgtitle = $(this).data("title");
+        $("#selectedimgtitle").val(imgtitle);
+        $("#selectedimgtitle").attr("name", imgid + "_Title");
+        $("#selectedimgcaption").val(imgcaption);
+        $("#selectedimgcaption").attr("name", imgid + "_Caption");
+        $("#selectedimgsrc").attr("src", imgsrc);
+    });
+});
